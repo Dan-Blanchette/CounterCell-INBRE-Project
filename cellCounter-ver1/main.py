f6abec5f6cc63fb2af2a224c7a93706e3a9e7818
Dan-Blanchette <blan5568@vandals.uidaho.edu>
2022-07-21 20:10:50 -0700

    EDIT: updated import due to file name change.

--- cellCounter-ver1/main.py
+++ cellCounter-ver1/main.py
@@ -13,7 +13,7 @@ This source code is licensed under the BSD-style license found in the
 LICENSE file in the root directory of this source tree. 
 """
 
-import cellCount as count
+import counterCell as count
     
 
 # main Function
